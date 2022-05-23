d0f1a0a2250e177ebfae1a8cc91696365d306524
Gergely Zalai <zgsbrgr@ZG-MBP14.home>
2022-05-23 16:38:26 +0200

    network core module:

---
+++ core-network/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("soulsession.android.library")
+    id("soulsession.android.library.jacoco")
+    kotlin("kapt")
+    id("kotlinx-serialization")
+    id("dagger.hilt.android.plugin")
+    id("soulsession.spotless")
+}
+
+
+dependencies {
+
+    implementation(project(":core-common"))
+    implementation(project(":core-model"))
+
+    testImplementation(project(":core-testing"))
+
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.serialization.json)
+
+    implementation(libs.okhttp.logging)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+}
