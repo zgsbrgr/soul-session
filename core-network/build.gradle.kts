5f1a3956c88cce0c9c2741e18c5533a13c0cdf59
Gergely Zalai <zgsbrgr@ZG-MBP14.home>
2022-05-24 09:25:38 +0200

    secrets gradle plugin added

--- core-network/build.gradle.kts
+++ core-network/build.gradle.kts
@@ -5,8 +5,13 @@ plugins {
     id("kotlinx-serialization")
     id("dagger.hilt.android.plugin")
     id("soulsession.spotless")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
+secrets {
+    propertiesFileName = "local.properties"
+    defaultPropertiesFileName="secrets.defaults.properties"
+}
 
 dependencies {
 
@@ -15,11 +20,13 @@ dependencies {
 
     testImplementation(project(":core-testing"))
 
+
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.kotlinx.serialization.json)
 
     implementation(libs.okhttp.logging)
     implementation(libs.retrofit.core)
+
     implementation(libs.retrofit.kotlin.serialization)
 
     implementation(libs.hilt.android)
