7bcf54944911baa214b4407d14d3f25b0bfa4fd9
gerg <zgsbrgr@ZG-MBP14.home>
2022-05-24 10:58:16 +0200

    spotless apply

--- core-common/src/main/java/com/zgsbrgr/soulsession/core/common/result/Result.kt
+++ core-common/src/main/java/com/zgsbrgr/soulsession/core/common/result/Result.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 zgsbrgr
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.zgsbrgr.soulsession.core.common.result
 
 import kotlinx.coroutines.flow.Flow
@@ -6,9 +22,9 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 
 sealed interface Result<out T> {
-    data class Success<T>(val data: T): Result<T>
-    data class Error(val exception: Throwable? = null): Result<Nothing>
-    object Loading: Result<Nothing>
+    data class Success<T>(val data: T) : Result<T>
+    data class Error(val exception: Throwable? = null) : Result<Nothing>
+    object Loading : Result<Nothing>
 }
 
 fun <T> Flow<T>.asResult(): Flow<Result<T>> {
@@ -23,5 +39,4 @@ fun <T> Flow<T>.asResult(): Flow<Result<T>> {
         .catch {
             emit(Result.Error(it))
         }
-
 }
--- core-network/src/main/java/com/zgsbrgr/soulsession/core/network/api/RetrofitSoulSessionNetwork.kt
+++ core-network/src/main/java/com/zgsbrgr/soulsession/core/network/api/RetrofitSoulSessionNetwork.kt
@@ -1,40 +1,50 @@
+/*
+ * Copyright 2022 zgsbrgr
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.zgsbrgr.soulsession.core.network.api
-
-
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.zgsbrgr.soulsession.core.network.BuildConfig
 import com.zgsbrgr.soulsession.core.network.SoulSessionNetwork
 import com.zgsbrgr.soulsession.core.network.model.NetworkEpisode
+import javax.inject.Inject
+import javax.inject.Singleton
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-
 import retrofit2.http.GET
-import javax.inject.Inject
-import javax.inject.Singleton
 
 interface SoulSessionNetworkApi {
 
     @GET
     suspend fun getEpisodes(): List<NetworkEpisode>
-
 }
 
 private const val BaseUrl = BuildConfig.BASEURL
 
 @Serializable
-private  data class NetworkResponse<T>(
+private data class NetworkResponse<T>(
     val data: T
 )
 
 @Singleton
-class RetrofitSoulSessionNetwork @Inject constructor(
-
-): SoulSessionNetwork {
+class RetrofitSoulSessionNetwork @Inject constructor() : SoulSessionNetwork {
 
     private val networkApi = Retrofit.Builder()
         .baseUrl(BaseUrl)
@@ -53,5 +63,4 @@ class RetrofitSoulSessionNetwork @Inject constructor(
 
     override suspend fun getEpisodes(): List<NetworkEpisode> =
         networkApi.getEpisodes()
-
 }
