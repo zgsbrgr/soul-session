0f73bff79866fdfcf021162bb3dbc8621273ab9f
gerg <zgsbrgr@ZG-MBP14.home>
2022-06-23 17:27:08 +0200

    implemented application wide event bus, currently handling topic selection from episode view

---
+++ core-common/src/main/java/com/zgsbrgr/soulsession/core/common/events/AppEventBus.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 zgsbrgr
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.zgsbrgr.soulsession.core.common.events
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class AppEventBus {
+
+    private val _selectedTopic = MutableStateFlow<Any?>(null)
+    val selectedTopic = _selectedTopic.asStateFlow()
+
+    fun <T> postMessage(message: T) {
+        _selectedTopic.value = message
+    }
+}
+
+sealed interface PlaybackState {
+    data class Playing(val item: Any) : PlaybackState
+    data class Paused(val item: Any) : PlaybackState
+    object Stopped : PlaybackState
+}
+
+data class PlaybackStateData(
+    val itemPlaybackState: PlaybackState = PlaybackState.Stopped
+)
