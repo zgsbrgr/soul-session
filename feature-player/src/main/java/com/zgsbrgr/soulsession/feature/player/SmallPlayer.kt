67b8511e4990a11551b88ade21fdd8b82076a174
gerg <zgsbrgr@ZG-MBP14.home>
2022-07-06 11:30:30 +0200

    unused imports cleared

--- feature-player/src/main/java/com/zgsbrgr/soulsession/feature/player/SmallPlayer.kt
+++ feature-player/src/main/java/com/zgsbrgr/soulsession/feature/player/SmallPlayer.kt
@@ -22,7 +22,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
