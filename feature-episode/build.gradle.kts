7ed6b773e5b70fdd80fbe9f0a4760409a58b4303
zgsbrgr <zgsbrgr@gmail.com>
2022-11-18 13:15:00 +0100

    these all already moved to feature convention plugin

--- feature-episode/build.gradle.kts
+++ feature-episode/build.gradle.kts
@@ -18,8 +18,6 @@ plugins {
     id("soulsession.android.feature")
     id("soulsession.android.library.compose")
     id("soulsession.android.library.jacoco")
-    kotlin("kapt")
-    id("dagger.hilt.android.plugin")
     id("soulsession.spotless")
 }
 
@@ -27,36 +25,7 @@ plugins {
 
 dependencies {
 
-    implementation(project(":core-model"))
-    implementation(project(":core-ui"))
-    implementation(project(":core-data"))
-    implementation(project(":core-common"))
-    implementation(project(":core-navigation"))
     implementation(project(":core-media"))
-
-    testImplementation(project(":core-testing"))
-    androidTestImplementation(project(":core-testing"))
-
     implementation(libs.androidx.media)
 
-    implementation(libs.kotlinx.coroutines.android)
-
-    implementation(libs.androidx.hilt.navigation.compose)
-    implementation(libs.androidx.lifecycle.viewModelCompose)
-
-    implementation(libs.hilt.android)
-    kapt(libs.hilt.compiler)
-
-    // TODO : Remove this dependency once we upgrade to Android Studio Dolphin b/228889042
-    // These dependencies are currently necessary to render Compose previews
-    debugImplementation(libs.androidx.customview.poolingcontainer)
-
-    // androidx.test is forcing JUnit, 4.12. This forces it to use 4.13
-    configurations.configureEach {
-        resolutionStrategy {
-            force(libs.junit4)
-            // Temporary workaround for https://issuetracker.google.com/174733673
-            force("org.objenesis:objenesis:2.6")
-        }
-    }
 }
